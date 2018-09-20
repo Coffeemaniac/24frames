3de0db46419769bdc6d3d1f6cd8fbf7e32934eff
Vachan <vachaneagleman@gmail.com>
2018-09-21 04:39:35 +0530

    initial commit

---
+++ app/src/main/java/com/example/vachan/a24frames/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.vachan.a24frames;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
